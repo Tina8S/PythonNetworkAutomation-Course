1cb12b47f3a72c8c2731c039e471e3f48e2be99b
Tina Strauf <tina.strauf@tu-braunschweig.de>
2018-03-26 00:54:06 -0700

    working version for exercise 2

--- Class2/telnet.py
+++ Class2/telnet.py
@@ -15,10 +15,6 @@ USER = "pyclass"
 PASS = "88newclass"
 
 
-if __name__ == "__main__":
-    main()
-
-
 def connect(ip_addr, TELNET_PORT, TELNET_TIMEOUT):
     try:
         return telnetlib.Telnet(ip_addr, TELNET_PORT, TELNET_TIMEOUT)
@@ -28,16 +24,41 @@ def connect(ip_addr, TELNET_PORT, TELNET_TIMEOUT):
 
 
 def login(conn, user, passw):
-    conn.read_until("sername:", TELNET_TIMEOUT)
+    conn.read_until("sername:".encode('utf-8'), TELNET_TIMEOUT)
     sleep(1)
-    comm.write(user + "\n")
-    conn.read_until("assword:", TELNET_TIMEOUT)
+    my_user = (user + "\n").encode('utf-8')
+    conn.write(my_user)
+    conn.read_until("assword:".encode('utf-8'), TELNET_TIMEOUT)
     sleep(1)
-    conn.write(passw + "\n")
-    output = conn.read_until("pynet-rtr#", TELNET_TIMEOUT)
-    print(output)
+    my_pass = (passw + "\n").encode('utf-8')
+    conn.write(my_pass)
+    output = conn.read_until("pynet-rtr#".encode('utf-8'), TELNET_TIMEOUT)
+    print(output.decode('utf-8'))
+
+
+def send_command(conn, command):
+    my_cmd = (command.rstrip() + "\n").encode('utf8')
+    conn.write(my_cmd)
+    sleep(1)
+    try:
+        return conn.read_very_eager()
+    except EOFError:
+        print("Something went wrong during execution of this command")
+        return ""
+
+
+def set_terminal_length(conn, length):
+    my_cmd = "set terminal length " + str(length)
+    send_command(conn, my_cmd)
 
 
 def main():
-    my_conn = connect(IP_ADDR, TELNET_PORT, TELNET_TIEMOUT)
+    my_conn = connect(IP_ADDR, TELNET_PORT, TELNET_TIMEOUT)
     login(my_conn, USER, PASS)
+    set_terminal_length(my_conn, 0)
+    sleep(1)
+    print(send_command(my_conn, "show ip int brief").decode('utf-8'))
+
+
+if __name__ == "__main__":
+    main()
